c0a25f64f98a9e2b8df1770dd4f9a7366c763f90
matcool <26722564+matcool@users.noreply.github.com>
2022-07-06 21:43:04 -0300

    possibly fix startup issues and fps label not showing up, update cocos-headers

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,8 @@ float get_active_fps_limit() {
 }
 
 void CCDirector_drawScene(cocos2d::CCDirector* self) {
-	if (!enabled) {
+	// disable for first 300 frames of game being open
+	if (!enabled || self->getTotalFrames() < 300) {
 		return matdash::orig<&CCDirector_drawScene>(self);
 	}
 
@@ -353,49 +354,6 @@ public:
 
 cocos2d::CCLabelBMFont* fps_label = nullptr;
 
-bool PlayLayer_init(PlayLayer* self, GJGameLevel* lvl) {
-	if (!matdash::orig<&PlayLayer_init>(self, lvl)) return false;
-
-	if (show_fps) {
-		const auto win_size = cocos2d::CCDirector::sharedDirector()->getWinSize();
-
-		auto label = cocos2d::CCLabelBMFont::create("some fps", "bigFont.fnt");
-		label->setOpacity(100);
-		label->setScale(0.4f);
-		label->setPosition({100, 100});
-		const float pad = 5.f;
-		switch (fps_corner) {
-			case Corner::TopLeft:
-				label->setAnchorPoint({0.f, 1.f});
-				label->setPosition({pad, win_size.height - pad + 4.f});
-				break;
-			case Corner::TopRight:
-				label->setAnchorPoint({1.f, 1.f});
-				label->setPosition({win_size.width - pad, win_size.height - pad + 4.f});
-				break;
-			case Corner::BotLeft:
-				label->setAnchorPoint({0.f, 0.f});
-				label->setPosition({pad, pad});
-				break;
-			case Corner::BotRight:
-				label->setAnchorPoint({1.f, 0.f});
-				label->setPosition({win_size.width - pad, pad});
-				break;
-		}
-
-		label->setZOrder(99);
-		self->addChild(label);
-
-		fps_label = label;
-		self->addChild(OnLeaveNode::create([] {
-			fps_label = nullptr;
-		}));
-	}
-	
-
-	return true;
-}
-
 std::chrono::time_point<std::chrono::high_resolution_clock> previous_frame, last_update;
 float avg = 0.f;
 int frame_count = 0;
@@ -403,24 +361,62 @@ int frame_count = 0;
 matdash::cc::thiscall<void> PlayLayer_update(PlayLayer* self, float dt) {
 	matdash::orig<&PlayLayer_update>(self, dt);
 
-	const auto now = std::chrono::high_resolution_clock::now();
+	if (show_fps) {
+		if (!fps_label) {
+			const auto win_size = cocos2d::CCDirector::sharedDirector()->getWinSize();
 
-	const std::chrono::duration<float> diff = now - previous_frame;
-	avg += diff.count();
-	frame_count++;
+			auto label = cocos2d::CCLabelBMFont::create("some fps", "bigFont.fnt");
+			label->setOpacity(100);
+			label->setScale(0.4f);
+			label->setPosition({100, 100});
+			const float pad = 5.f;
+			switch (fps_corner) {
+				case Corner::TopLeft:
+					label->setAnchorPoint({0.f, 1.f});
+					label->setPosition({pad, win_size.height - pad + 4.f});
+					break;
+				case Corner::TopRight:
+					label->setAnchorPoint({1.f, 1.f});
+					label->setPosition({win_size.width - pad, win_size.height - pad + 4.f});
+					break;
+				case Corner::BotLeft:
+					label->setAnchorPoint({0.f, 0.f});
+					label->setPosition({pad, pad});
+					break;
+				case Corner::BotRight:
+					label->setAnchorPoint({1.f, 0.f});
+					label->setPosition({win_size.width - pad, pad});
+					break;
+			}
 
-	if (fps_label && std::chrono::duration<float>(now - last_update).count() > 1.0f) {
-		last_update = now;
-		const auto fps = static_cast<int>(std::roundf(static_cast<float>(frame_count) / avg));
-		avg = 0.f;
-		frame_count = 0;
+			label->setZOrder(99);
+			self->addChild(label);
 
-		std::stringstream stream;
-		stream << fps << " rFPS";
-		fps_label->setString(stream.str().c_str());
+			fps_label = label;
+			self->addChild(OnLeaveNode::create([] {
+				fps_label = nullptr;
+			}));
+		}
+
+		const auto now = std::chrono::high_resolution_clock::now();
+
+		const std::chrono::duration<float> diff = now - previous_frame;
+		avg += diff.count();
+		frame_count++;
+
+		if (std::chrono::duration<float>(now - last_update).count() > 1.0f) {
+			last_update = now;
+			const auto fps = static_cast<int>(std::roundf(static_cast<float>(frame_count) / avg));
+			avg = 0.f;
+			frame_count = 0;
+
+			std::stringstream stream;
+			stream << fps << " rFPS";
+			fps_label->setString(stream.str().c_str());
+		}
+
+		previous_frame = now;
 	}
-
-	previous_frame = now;
 
 	return {};
 }
@@ -432,7 +428,6 @@ void mod_main(HMODULE) {
 
 	matdash::add_hook<&MyMoreVideoOptionsLayer::init_>(gd::base + 0x1E2590);
 
-	matdash::add_hook<&PlayLayer_init>(base + 0x1FB780);
 	matdash::add_hook<&PlayLayer_update>(base + 0x2029C0);
 
 	load_gv_values();
